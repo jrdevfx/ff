f652c8e41fe10706b47ec0e74f90fe40a519118b
jrdevfx <140286013+jrdevfx@users.noreply.github.com>
2023-07-23 00:39:05 +0100

    Create mtt.ps1

---
+++ mtt.ps1
@@ -0,0 +1,17 @@
+$agentsInstallerUrl = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5tester.setup.exe'
+$agentsInstallerOutFile = 'C:\Users\joshua24\Downloads\mt5tester.setup.exe'
+$agentsInstallerPath = 'C:\Program Files\MetaTrader 5 Strategy Tester\MetaTester64.exe'
+$password = 'MetaTester'
+$startingPort = 2000
+$numberOfAgents = 96
+
+Invoke-WebRequest -Uri $agentsInstallerUrl -OutFile $agentsInstallerOutfile
+Start-Process -FilePath $agentsInstallerOutFile /silent -Wait
+
+for ($i = 1; $i -le $numberOfAgents; $i++) {
+    $port = $startingPort + $i - 1
+    Start-Process -FilePath $agentsInstallerPath -ArgumentList "/install", "/silent", "/password:$password", "/address:0.0.0.0:$port" -Wait
+    Start-Process -FilePath $agentsInstallerPath -ArgumentList "/start", "/silent", "/address:0.0.0.0:$port"
+    
+    #Start-Process -FilePath $agentsInstallerPath -ArgumentList "/uninstall", "/silent", "/address:0.0.0.0:$port"
+}
