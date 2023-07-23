ca038bfc846045ac7aca6bd61abfe035efa8142a
jrdevfx <140286013+jrdevfx@users.noreply.github.com>
2023-07-23 16:33:15 +0100

    Update mtt.ps1

--- mtt.ps1
+++ mtt.ps1
@@ -1,12 +1,12 @@
-$agentsInstallerUrl = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5tester.setup.exe'
-$agentsInstallerOutFile = 'C:\Temp\mt5tester.setup.exe'
-$agentsInstallerPath = 'C:\Program Files\MetaTrader 5 Strategy Tester\MetaTester64.exe'
+$agentsInstallerUrl = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe'
+$agentsInstallerOutFile = 'C:\Temp\mt5setup.exe'
+$agentsInstallerPath = 'C:\Program Files\MetaTrader 5\metatester64.exe'
 $password = 'MetaTester'
 $startingPort = 2000
 $numberOfAgents = 96
 
 Invoke-WebRequest -Uri $agentsInstallerUrl -OutFile $agentsInstallerOutfile -UseBasicParsing
-Start-Process -FilePath $agentsInstallerOutFile /silent -Wait
+Start-Process -FilePath $agentsInstallerOutFile /auto -Wait
 
 Start-Sleep -Seconds 15
 
