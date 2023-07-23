a8ac2ec222cbb93dec2297fd1a45156a0c794db9
jrdevfx <140286013+jrdevfx@users.noreply.github.com>
2023-07-23 23:10:45 +0100

    Update mt5-net.ps1

--- mt5-net.ps1
+++ mt5-net.ps1
@@ -4,5 +4,6 @@ $agentsInstallerOutFile = 'C:\Temp\mt5setup.exe'
 Invoke-WebRequest -Uri $agentsInstallerUrl -OutFile $agentsInstallerOutfile -UseBasicParsing
 Start-Process -FilePath $agentsInstallerOutFile /auto -Wait
 
+
+Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/035efed3-6386-4e1d-bcbc-384a20ebf47e/abfbea2303e0ce9cb15d430314e5858f/windowsdesktop-runtime-6.0.14-win-x64.exe -OutFile C:\Temp\windowsdesktop-runtime-6.0.14-win-x64.exe -UseBasicParsing
 Start-Process -FilePath C:\Temp\windowsdesktop-runtime-6.0.14-win-x64.exe /silent -Wait
-Start-Process -FilePath C:\Temp\windowsdesktop-runtime-6.0.14-win-x64.exe -Wait
